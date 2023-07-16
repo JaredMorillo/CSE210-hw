9b8e41b466869300a6a875414b84d30fda8e0eb1
Jared Morillo <jared.morillo@hotmail.com>
2023-07-16 01:27:00 -0500

    Foundation 4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -8,9 +8,9 @@ class Program
         Address address2 = new Address("456 Elm St", "Townsville", "Stateville", "Countryland");
         Address address3 = new Address("789 Oak St", "Villageland", "Stateville", "Countryland");
 
-        Lecture lecture = new Lecture("Interesting Lecture", "Harry Potter and The Lord of the Rings.", new DateTime(2023, 7, 20), new TimeSpan(18, 30, 0), address1, "Jared Morillo", 100);
-        Reception reception = new Reception("Networking Reception", "Join us for a networking event.", new DateTime(2023, 7, 22), new TimeSpan(19, 0, 0), address2, "jm45@gmail.com");
-        OutdoorGathering outdoorGathering = new OutdoorGathering("Summer Picnic", "Enjoy a fun-filled picnic in the park.", new DateTime(2023, 7, 25), new TimeSpan(12, 0, 0), address3, "Sunny skies expected!");
+        Event lecture = new Lecture("Interesting Lecture", "An informative lecture about a fascinating topic.", new DateTime(2023, 7, 20), new TimeSpan(18, 30, 0), address1, "Jared Morillo", 100);
+        Event reception = new Reception("Networking Reception", "Join us for a networking event.", new DateTime(2023, 7, 22), new TimeSpan(19, 0, 0), address2, "jm45@gmail.com");
+        Event outdoorGathering = new OutdoorGathering("Summer Picnic", "Enjoy a fun-filled picnic in the park.", new DateTime(2023, 7, 25), new TimeSpan(12, 0, 0), address3, "Sunny skies expected!");
 
         Console.WriteLine("=== Standard Details ===");
         Console.WriteLine(lecture.GenerateStandardDetails());
@@ -59,11 +59,11 @@ class Address
 
 class Event
 {
-    private string title;
-    private string description;
-    private DateTime date;
-    private TimeSpan time;
-    private Address address;
+    protected string title;
+    protected string description;
+    protected DateTime date;
+    protected TimeSpan time;
+    protected Address address;
 
     public Event(string title, string description, DateTime date, TimeSpan time, Address address)
     {
@@ -74,7 +74,7 @@ class Event
         this.address = address;
     }
 
-    public string GenerateStandardDetails()
+    public virtual string GenerateStandardDetails()
     {
         return $"Title: {title}\nDescription: {description}\nDate: {date.ToShortDateString()}\nTime: {time.ToString()}\nAddress: {address.ToString()}";
     }
